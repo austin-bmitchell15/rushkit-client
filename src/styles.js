00ef3ebe62e36fd501efdae07fc920ee677dc51f
austin <amitchell80@gatech.edu>
2022-08-01 17:21:55 -0400

    Integrated MongoDB. Need to fix issue with posts not being retrieved on reload.

--- src/styles.js
+++ src/styles.js
@@ -4,14 +4,14 @@ import { makeStyles } from "@material-ui/core/styles";
 export default makeStyles(() => ({
     appBar: {
       borderRadius: 15,
-      margin: '30px 0',
+      margin: '30px',
       display: 'flex',
       flexDirection: 'row',
       justifyContent: 'center',
       alignItems: 'center',
     },
     heading: {
-      color: 'rgba(0,183,255, 1)',
+      color: 'rgba(121, 11, 24, 0.8)',
     },
     image: {
       marginLeft: '15px',
