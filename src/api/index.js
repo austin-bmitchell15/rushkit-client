e7c3b4197bf4540e7f8926b423c3accada210688
austin-bmitchell15 <austin.bmitchell15@gmail.com>
2022-08-18 17:26:23 -0400

    Got rid of random console logs.

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://rushkit-project.herokuapp.com' });
+const API = axios.create({ baseURL: 'https://rushkit-server.herokuapp.com/' });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
